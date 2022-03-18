3af171a5203b3ac31e0c04588c1cd408e3c7f421
sumer5020 <sumer5020@gmail.com>
2022-03-18 16:11:43 +0300

    up

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/'
-    : '/public/'
-}
+module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+  transpileDependencies: true
+})
